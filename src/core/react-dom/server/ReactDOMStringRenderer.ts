f08358a043d684b1d8c98a1a778aa1a2d779b3aa
saber2pr <saber2pr@gmail.com>
2019-12-08 12:20:53 +0000

    feat(ReactDOMServer): fragment match.

--- src/core/react-dom/server/ReactDOMStringRenderer.ts
+++ src/core/react-dom/server/ReactDOMStringRenderer.ts
@@ -2,14 +2,20 @@
  * @Author: saber2pr
  * @Date: 2019-12-07 22:09:45
  * @Last Modified by: saber2pr
- * @Last Modified time: 2019-12-07 23:08:10
+ * @Last Modified time: 2019-12-08 12:17:36
  */
-import { isTextFiber, isHookFiber, isHostFiber } from "../../react-is/ReactIs"
+import {
+  isTextFiber,
+  isHookFiber,
+  isHostFiber,
+  isFragmentFiber
+} from "../../react-is/ReactIs"
+import { Children } from "../../react/ReactChildren"
 
 const toLowerCase = (value: string) =>
   value.replace(/\B([A-Z])/g, "-$1").toLowerCase()
 
-const toArray = (element: any) => Array.prototype.concat(element)
+const toArray = (element: any) => Children.toArray(element)
 
 function createNode(element: JSX.Element): JSX.Element {
   const { props } = element
@@ -47,6 +53,12 @@ function renderToString(element: JSX.Element): string {
 
     const children = toArray(props.children)
     return `<${tag}${attr}>${children.map(renderToString).join("")}</${tag}>`
+  }
+
+  if (isFragmentFiber(element)) {
+    const { props } = element
+    const children = toArray(props.children)
+    return children.map(renderToString).join("")
   }
 
   if (isHookFiber(element)) {
