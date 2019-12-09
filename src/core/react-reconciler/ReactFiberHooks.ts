66cf9c7b98effd0f22196989d951bea6ce7f8274
saber2pr <saber2pr@gmail.com>
2019-12-09 20:52:45 +0000

    feat(ReactFiberHooks): add useEffect Hook

--- src/core/react-reconciler/ReactFiberHooks.ts
+++ src/core/react-reconciler/ReactFiberHooks.ts
@@ -2,17 +2,18 @@
  * @Author: saber2pr
  * @Date: 2019-12-06 17:11:09
  * @Last Modified by: saber2pr
- * @Last Modified time: 2019-12-07 15:19:09
+ * @Last Modified time: 2019-12-08 14:11:29
  */
 import { getCurrentWorkInProgress } from "./ReactFiberWorkLoop"
 import { scheduleWork } from "./ReactFiberReconciler"
 import { getIndex } from "./ReactFiberStack"
 import { is } from "../shared/objectIs"
+import { Effect, MemoizedState } from "../shared/ReactTypes"
 
-function areHookInputsEqual(
+const areHookInputsEqual = (
   nextDeps: readonly any[],
   prevDeps: readonly any[] | null
-) {
+) => {
   if (prevDeps === null) {
     return false
   }
@@ -25,11 +26,11 @@ function areHookInputsEqual(
   return true
 }
 
-function useState<T>(initialState: T): [T, (state: T) => void] {
+const useState = <T>(initialState: T): [T, (state: T) => void] => {
   const id = getIndex()
   const fiber = getCurrentWorkInProgress()
 
-  const memoizedState: object = fiber.memoizedState || {}
+  const memoizedState: MemoizedState = fiber.memoizedState || {}
   if (!(id in memoizedState)) {
     memoizedState[id] = initialState
     fiber.memoizedState = memoizedState
@@ -58,15 +59,15 @@ const useReducer = <S, A>(
   return [state, dispatch]
 }
 
-function useRef<T>(value?: T) {
+const useRef = <T>(value?: T) => {
   const [state] = useState({ current: value })
   return state
 }
 
-function useCallBack<T extends Function>(
+const useCallBack = <T extends Function>(
   callback: T,
   deps: readonly any[] = []
-) {
+) => {
   const ref = useRef(callback)
   const prevDepsRef = useRef(null)
   if (areHookInputsEqual(deps, prevDepsRef.current)) {
@@ -78,7 +79,7 @@ function useCallBack<T extends Function>(
   }
 }
 
-function useMemo<T>(memoFunc: () => T, deps: readonly any[] = []) {
+const useMemo = <T>(memoFunc: () => T, deps: readonly any[] = []) => {
   const ref = useRef<T>(null)
   const prevDepsRef = useRef(null)
   if (areHookInputsEqual(deps, prevDepsRef.current)) {
@@ -90,4 +91,23 @@ function useMemo<T>(memoFunc: () => T, deps: readonly any[] = []) {
   }
 }
 
-export { useCallBack, useMemo, useReducer, useRef, useState }
+const pushEffect = (effect: MemoizedState | null, create: Effect) => {
+  const sideEffect = effect || {}
+  const updateQueue = sideEffect.in || []
+  updateQueue.push(create)
+  sideEffect.in = updateQueue
+  return sideEffect
+}
+
+const useEffect = (create: Effect, deps?: readonly any[]) => {
+  const fiber = getCurrentWorkInProgress()
+  const effect = fiber.memoizedState
+  const prevDepsRef = useRef(null)
+  if (areHookInputsEqual(deps, prevDepsRef.current)) {
+    return
+  } else {
+    fiber.memoizedState = pushEffect(effect, create)
+  }
+}
+
+export { useCallBack, useMemo, useReducer, useRef, useState, useEffect }
