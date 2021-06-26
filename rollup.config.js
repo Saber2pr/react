12ac93f12216ec02bd00c5f373d5c1f8e0ffac2f
saber2pr <saber2pr@gmail.com>
2021-06-26 22:34:14 +0800

    chore: build

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,23 @@
 import resolve from "rollup-plugin-node-resolve"
 import commonjs from "rollup-plugin-commonjs"
 import { uglify } from "rollup-plugin-uglify"
+import html from '@rollup/plugin-html'
+
+const cdn = '//cdn.jsdelivr.net/gh'
+const username = 'saber2pr'
+const repo = 'react'
+const pages_branch = 'gh-pages'
+
+const publicPath = process.env.NODE_ENV === 'production'
+  ? `${cdn}/${username}/${repo}@${pages_branch}/`
+  : './'
+
+const fileName = `bundle.${Date.now()}.js`
 
 export default {
   input: "./lib/__test__/Test.js",
   output: {
-    file: "./build/bundle.js",
+    file: `./build/${fileName}`,
     format: "iife",
     name: "react",
     exports: "named"
@@ -13,5 +25,16 @@ export default {
   watch: {
     include: "lib/**"
   },
-  plugins: [commonjs(), resolve(), uglify()]
+  plugins: [commonjs(), resolve(), uglify(), html({
+    template: () => `<!DOCTYPE html>
+    <html>
+      <head>
+        <title>@saber2pr/react</title>
+      </head>
+      <body>
+        <div id="root"></div>
+        <script src=${`${publicPath}${fileName}`}></script>
+      </body>
+    </html>`
+  })]
 }
